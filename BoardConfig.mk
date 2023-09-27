c662527203855ebe0246fb918071e414810c8263
me-cafebabe <me.cafebabe@gmail.com>
2023-09-27 15:00:11 +0800

    Android.bp: Add gralloc hal related defaults from lineage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,3 +7,13 @@ include $(HW_MITHORIUM_AOSP_PATH)/qcom_boards.mk
 include $(HW_MITHORIUM_AOSP_PATH)/qcom_target.mk
 
 include $(HW_MITHORIUM_AOSP_PATH)/BoardConfigQcom.mk
+
+# Soong config
+SOONG_CONFIG_NAMESPACES += lineageGlobalVars
+SOONG_CONFIG_lineageGlobalVars := \
+    gralloc_handle_has_custom_content_md_reserved_size \
+    gralloc_handle_has_reserved_size
+
+# Soong bool variables
+SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
+SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
