8ebab329b3d75c2f77fc684b4edb0b3c6727e503
Arian <arian.kulmer@web.de>
2023-11-04 10:49:49 +0100

    soong: Add qti vibrator effect stream config
    Change-Id: I87b08a02f91a941d22dab5d1acb9bfbbce06f03b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,11 +9,21 @@ include $(HW_MITHORIUM_AOSP_PATH)/qcom_target.mk
 include $(HW_MITHORIUM_AOSP_PATH)/BoardConfigQcom.mk
 
 # Soong config
-SOONG_CONFIG_NAMESPACES += lineageGlobalVars
+SOONG_CONFIG_NAMESPACES += lineageGlobalVars lineageQcomVars
 SOONG_CONFIG_lineageGlobalVars := \
     gralloc_handle_has_custom_content_md_reserved_size \
     gralloc_handle_has_reserved_size
+SOONG_CONFIG_lineageQcomVars += \
+    qti_vibrator_effect_lib \
+    qti_vibrator_use_effect_stream
+
+# Set default values
+TARGET_QTI_VIBRATOR_EFFECT_LIB ?= libqtivibratoreffect
 
 # Soong bool variables
 SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_custom_content_md_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_CUSTOM_CONTENT_MD_RESERVED_SIZE)
 SOONG_CONFIG_lineageGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
+SOONG_CONFIG_lineageQcomVars_qti_vibrator_use_effect_stream := $(TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM)
+
+# Soong value variables
+SOONG_CONFIG_lineageQcomVars_qti_vibrator_effect_lib := $(TARGET_QTI_VIBRATOR_EFFECT_LIB)
